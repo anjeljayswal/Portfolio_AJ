d90336d720961db2641f34bf140c88d2ef123b6a
anjeljayswal <anjeljayswal@gmail.com>
2023-06-09 07:33:31 +0545

    heroImg2

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import HeroImg2 from '../Components/HeroImg2'
 
 const About = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="ABOUT." text="I am a friendly Front-End Developer." />
+      
       <Footer />
     </div>
   )
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import HeroImg2 from '../Components/HeroImg2'
 
 const Project = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2 heading="PROJECTS." text="Some of my most recent works" />
       <Footer />
     </div>
   )
