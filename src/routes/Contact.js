88832259cd67b2da9479dcd9830ee89f75e73a8f
anjeljayswal <anjeljayswal@gmail.com>
2023-06-09 06:46:58 +0545

    footer-page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 const Contact = () => {
   return (
     <div>
-      Cont
+      <Navbar />
+      <Footer />
     </div>
   )
 }
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
 
 const Project = () => {
   return (
     <div>
-      Project
+      <Navbar />
+      <Footer />
     </div>
   )
 }
